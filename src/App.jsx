8009ac851839d0cd659ce4aaf675162e7e80de95
Priya Gupta <priyagupta052k@gmail.com>
2025-04-26 23:33:31 +0530

    Initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Navbar from '../src/components/Navbar';
-import About from '../src/components/About';
 function App() {
 
   return (
     <>
       <h1>Codefolio - Portfolio</h1>
-      <Navbar />
-      <About />
+      <Homepage />
     </>
   )
 }
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+function Homepage()
+{
+    return(
+        <>
+            <Navbar />
+            <About />
+            <Skills />
+            <Projects />
+        </>
+    );
+}
