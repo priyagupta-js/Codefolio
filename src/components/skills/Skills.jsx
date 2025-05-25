a035170bb288b675639572c7006a2749a3719ec8
Priya Gupta <priyagupta052k@gmail.com>
2025-05-25 23:57:07 +0530

    Modified Navbar

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Skills ()
 {
     return(
-<>
+<div>
     Skills
-</>
+</div>
     );
 }
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar/Navbar";
 import About from "../components/About/About";
-import Skills from "../components/Skills";
+import Skills from "../components/skills/Skills";
 import Projects from "../components/Projects";
 import "../../src/index.css";
 function Homepage()
