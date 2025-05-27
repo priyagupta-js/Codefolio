a3549966f04bda0b44e438fabacb32f2929d861a
Priya Gupta <priyagupta052k@gmail.com>
2025-05-27 22:27:20 +0530

    Add About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,6 +5,10 @@ function About() {
       <div className="section-heading">
         <h1>About</h1>
       </div>
+      <div className="about-left">
+      </div>
+      <div className="about-right">
+      </div>
     </div>
   );
 }
