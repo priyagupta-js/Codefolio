e7e10757afa4cea70c8f42f0a67de058e6d9fb55
Priya Gupta <priyagupta052k@gmail.com>
2025-06-02 23:43:35 +0530

    Add Home section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,15 @@
+import "./Home.css";
 function Home()
 {
     return(
 <div className="Home-container">
+<div className="home-content">
     <h5>Hey!!</h5>
     <h1>I am Priya Gupta</h1>
     <h5>I am into <span>Mern Stack develpment ; Frontend Development ; Software Development</span></h5>
     <button>My Work</button>
     <button>Hire me</button>  
+    </div>
 </div>
     );
 }
